f33ebbaed61e2c6157811fb1f783b20ee9dd9df5
Ritvik S Raiker <130823208+heheimmastudent@users.noreply.github.com>
2023-11-19 01:10:13 +0530

    Update towerOfHanoi.s

--- towerOfHanoi.s
+++ towerOfHanoi.s
@@ -11,75 +11,75 @@ n:       .word   4
 .text
 .globl main
 main:
-    lw a1, n
-    la t0, src
-    la t1, dst
-    la t2, aux
-    lbu a2, 0(t0)
-    lbu a3, 0(t2)
-    lbu a4, 0(t1)
-    jal x1, towerOfHanoi
-    li a7, 10
-    ecall
+    lw a1, n           # Load the value of n (number of disks) into a1
+    la t0, src         # Load the address of source pole ('A') into t0
+    la t1, dst         # Load the address of destination pole ('C') into t1
+    la t2, aux         # Load the address of auxiliary pole ('B') into t2
+    lbu a2, 0(t0)      # Load the first character of source pole into a2
+    lbu a3, 0(t2)      # Load the first character of auxiliary pole into a3
+    lbu a4, 0(t1)      # Load the first character of destination pole into a4
+    jal x1, towerOfHanoi  # Jump and call the towerOfHanoi function
+    li a7, 10          # Load the system call number for program exit into a7
+    ecall              # Make the system call to exit the program
 
 towerOfHanoi:
-    addi sp, sp, -20
-    sw x1, 0(sp)
-    sw a1, 4(sp)
-    sw a2, 8(sp)
-    sw a3, 12(sp)
-    sw a4, 16(sp)
-    addi t0, x0, 1
-    beq a1, t0, return
-    lw a1, 4(sp)
-    addi a1, a1, -1
-    lbu a2, 8(sp)
-    lbu a3, 16(sp)
-    lbu a4, 12(sp)
-    jal x1, towerOfHanoi
-    lw a1, 4(sp)
-    lbu a2, 8(sp)
-    lbu a3, 16(sp)
-    jal x1, print
-    lw a1, 4(sp)
-    addi a1, a1, -1
-    lbu a2, 12(sp)
-    lbu a3, 8(sp)
-    lbu a4, 16(sp)
-    jal x1, towerOfHanoi
-    lw x1, 0(sp)
-    addi sp, sp, 20
-    jalr x0, x1, 0
+    addi sp, sp, -20    # Allocate space on the stack for local variables
+    sw x1, 0(sp)        # Save the return address on the stack
+    sw a1, 4(sp)        # Save a1 (number of disks) on the stack
+    sw a2, 8(sp)        # Save a2 (source pole) on the stack
+    sw a3, 12(sp)       # Save a3 (destination pole) on the stack
+    sw a4, 16(sp)       # Save a4 (auxiliary pole) on the stack
+    addi t0, x0, 1      # Load the value 1 into t0
+    beq a1, t0, return  # If a1 (number of disks) is 1, jump to the return label
+    lw a1, 4(sp)        # Load the value of a1 (number of disks) from the stack
+    addi a1, a1, -1     # Decrement a1 (number of disks) by 1
+    lbu a2, 8(sp)       # Load source pole from the stack
+    lbu a3, 16(sp)      # Load destination pole from the stack
+    lbu a4, 12(sp)      # Load auxiliary pole from the stack
+    jal x1, towerOfHanoi  # Recursive call to towerOfHanoi
+    lw a1, 4(sp)        # Load the value of a1 (number of disks) from the stack
+    lbu a2, 8(sp)       # Load source pole from the stack
+    lbu a3, 16(sp)      # Load destination pole from the stack
+    jal x1, print       # Call the print function
+    lw a1, 4(sp)        # Load the value of a1 (number of disks) from the stack
+    addi a1, a1, -1     # Decrement a1 (number of disks) by 1
+    lbu a2, 12(sp)      # Load auxiliary pole from the stack
+    lbu a3, 8(sp)       # Load source pole from the stack
+    lbu a4, 16(sp)      # Load destination pole from the stack
+    jal x1, towerOfHanoi  # Recursive call to towerOfHanoi
+    lw x1, 0(sp)        # Load the return address from the stack
+    addi sp, sp, 20     # Deallocate space on the stack
+    jalr x0, x1, 0      # Jump and link to the return address
 
 return:
-    lw a1, 4(sp)
-    lbu a2, 8(sp)
-    lbu a3, 16(sp)
-    jal x1, print
-    lw x1, 0(sp)
-    addi sp, sp, 20
-    jalr x0, x1, 0
+    lw a1, 4(sp)        # Load the value of a1 (number of disks) from the stack
+    lbu a2, 8(sp)       # Load source pole from the stack
+    lbu a3, 16(sp)      # Load destination pole from the stack
+    jal x1, print       # Call the print function
+    lw x1, 0(sp)        # Load the return address from the stack
+    addi sp, sp, 20     # Deallocate space on the stack
+    jalr x0, x1, 0      # Jump and link to the return address
 
 print:
-    la a0, md
-    li a7, 4
-    ecall
-    addi a0, a1, 0
-    li a7, 1
-    ecall
-    la a0, from
-    li a7, 4
-    ecall
-    addi a0, a2, 0
-    li a7, 11
-    ecall
-    la a0, to
-    li a7, 4
-    ecall
-    addi a0, a3, 0
-    li a7, 11
-    ecall
-    la a0, newline
-    li a7, 4
-    ecall
-    jalr x0, x1, 0
+    la a0, md           # Load the address of "Move Disk " into a0
+    li a7, 4            # Load the system call number for printing a string into a7
+    ecall               # Make the system call to print the string
+    addi a0, a1, 0      # Load the value of a1 (number of disks) into a0
+    li a7, 1            # Load the system call number for printing an integer into a7
+    ecall               # Make the system call to print the integer
+    la a0, from         # Load the address of " from '" into a0
+    li a7, 4            # Load the system call number for printing a string into a7
+    ecall               # Make the system call to print the string
+    addi a0, a2, 0      # Load the value of a2 (source pole) into a0
+    li a7, 11           # Load the system call number for printing a character into a7
+    ecall               # Make the system call to print the character
+    la a0, to           # Load the address of "' to '" into a0
+    li a7, 4            # Load the system call number for printing a string into a7
+    ecall               # Make the system call to print the string
+    addi a0, a3, 0      # Load the value of a3 (destination pole) into a0
+    li a7, 11           # Load the system call number for printing a character into a7
+    ecall               # Make the system call to print the character
+    la a0, newline      # Load the address of "'\n" into a0
+    li a7, 4            # Load the system call number for printing a string into a7
+    ecall               # Make the system call to print the string
+    jalr x0, x1, 0      # Jump and link to the return address
